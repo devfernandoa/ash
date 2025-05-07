c09cadbb88c7990b586d665b51b982a76161651c
devfernandoa <fefealzueta@gmail.com>
2025-05-07 20:22:42 -0300

    Updating actions

--- analysis/tests/run-tests.sh
+++ analysis/tests/run-tests.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# Go up to the analysis folder (where ash binary is)
+cd ..
+
 for file in tests/*.ash; do
     echo "Running $file"
     ./ash < "$file"
     if [ $? -ne 0 ]; then
         echo "❌ Error in $file"
+        exit 1
     else
         echo "✅ Success"
     fi
