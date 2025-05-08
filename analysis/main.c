f5394ca4b624df94010948e3b11d12f88bdafc27
devfernandoa <fefealzueta@gmail.com>
2025-05-07 21:35:35 -0300

    Updating main.c

--- analysis/main.c
+++ analysis/main.c
@@ -2,10 +2,6 @@
 
 int yyparse(void);
 
-void yyerror(const char* s) {
-    fprintf(stderr, "Error: %s\n", s);
-}
-
 int main() {
     int result = yyparse();
     return result;  // returns non-zero if there was a syntax error
