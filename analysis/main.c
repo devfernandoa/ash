d97073fbf00e81684c4addbaac76c608f4ae4f79
devfernandoa <fefealzueta@gmail.com>
2025-05-07 21:28:34 -0300

    Updating main.c

--- analysis/main.c
+++ analysis/main.c
@@ -4,5 +4,5 @@ int yyparse(void);
 
 int main() {
     int result = yyparse();
-    return result;  // ✅ returns non-zero if there was a syntax error
+    return result;  // returns non-zero if there was a syntax error
 }
